5bf26dfdfe42b08c2d9b9e1477f3d690914b78e4
Luca Fenu <luca.fenu01@gmail.com>
2022-12-28 09:29:21 +0100

    Added infinite loop

--- run.js
+++ run.js
@@ -5,18 +5,25 @@ var Influx = require('./influx');
 const influxObj = new Influx(config.influx);
 
 
-(async () => {
-  try {
-    // Check the internet speed
-    const speedTestValue = await speedTest(config.speedtest);
+async function run() {
+    try {
+        // Check the internet speed
+        const speedTestValue = await speedTest(config.speedtest);
 
-    // Write the data to Influx. Since the bandwidth is in bytes per second, it gets multiplied by 8 to get bits per second (bps)
-    await influxObj.write(config.speedtest.host, speedTestValue.ping.latency, speedTestValue.ping.jitter, speedTestValue.download.bandwidth * 8,
-      speedTestValue.upload.bandwidth * 8, speedTestValue.packetLoss)
+        // Write the data to Influx. Since the bandwidth is in bytes per second, it gets multiplied by 8 to get bits per second (bps)
+        await influxObj.write(config.speedtest.host, speedTestValue.ping.latency, speedTestValue.ping.jitter, speedTestValue.download.bandwidth * 8,
+            speedTestValue.upload.bandwidth * 8, speedTestValue.packetLoss);
+        
+        // Call self back
+        setTimeout(async () => {
+            await run();
+        }, 5000);
 
-  } catch (err) {
-    console.log(err.message);
-  } finally {
-    process.exit(0);
-  }
-})();
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+setTimeout(async () => {
+    await run();
+}, 5000);
