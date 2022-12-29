f08484a20164dad17fc53d0cf616c9e2c5ebca16
Luca Fenu <luca.fenu01@gmail.com>
2022-12-29 09:23:58 +0100

    Commented code

--- influx.js
+++ influx.js
@@ -3,6 +3,10 @@ const Point = require('@influxdata/influxdb-client').Point
 
 
 class Influx {
+    /**
+     * Initialize the Influx object
+     * @param {Object} config An object with the following paramerters: bucket, measurement, deviceHost, host, token
+     */
     constructor(config) {
         this.bucket = config.bucket;
         this.measurement = config.measurement;
@@ -11,6 +15,15 @@ class Influx {
         this.influxAPI = new InfluxDB({url: config.host, token: config.token})
     }
 
+    /**
+     * Write a point on the Influx DB
+     * @param {String} speedtestServer The host the speed was checked on (friendly name)
+     * @param {float} ping The ping measured
+     * @param {float} jitter The jitter measured
+     * @param {int} download The download speed in bps
+     * @param {int} upload The upload speed in bps
+     * @param {float} packetLoss The packet loss in percentage
+     */
     async write(speedtestServer, ping, jitter, download, upload, packetLoss) {
         const writeAPI = this.influxAPI.getWriteApi(this.org, this.bucket)
         const point = new Point(this.measurement)
