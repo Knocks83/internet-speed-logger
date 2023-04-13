f6d7818842addd1668c3128686f07f4b85e4f933
Luca Fenu <luca.fenu01@gmail.com>
2023-04-13 21:47:26 +0200

    Switched from Speedtest server host to server ID in the conf

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ config.general = {};
 
 config.speedtest.acceptGdpr = false;    // Read the SpeedTest.net GDPR and then set to true
 config.speedtest.acceptLicense = false; // Read the SpeedTest.net License and then set to true
-config.speedtest.host = "";             // It's better to find a server and always use that one, for consistency
+config.speedtest.id = "";               // It's better to find a server and always use that one, for consistency
 
 config.influx.host = "http://localhost:8086";
 config.influx.token = "";               // For 1.8 it's username:password
